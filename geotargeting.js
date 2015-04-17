2a68d853542efc5f0826d593c615fac895f71363
kaban4ik1994 <kaban4ik19901990@mail.ru>
2015-04-17 23:42:49 +0300

    Params in the main function was modified

--- geotargeting.js
+++ geotargeting.js
@@ -1,14 +1,14 @@
 var baseUrlApiGeotargeting = 'http://maps.googleapis.com/maps/api/geocode/json';
 var request = require('request');
 
-var geotargeting = function (options) {
+var geotargeting = function (options, callbackFunction) {
     var requestParams = {
         latlng: options.latlng,
         language: 'EN',
         sensor: false
     };
 
-    request({ url: baseUrlApiGeotargeting, qs: requestParams }, options.callbackFunction);
+    request({ url: baseUrlApiGeotargeting, qs: requestParams }, callbackFunction);
 };
 
 module.exports = geotargeting;
